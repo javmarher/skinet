b4f1f9599de20bceb5c2a92fa47a60daed7001d6
jmh <javiermartinezherranz@gmail.com>
2020-06-17 19:46:02 +0200

    Products Repository Finalizado

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -24,11 +24,27 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
         {
             var products =  await _repo.GetProductsAsync();
 
             return Ok(products);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            var productsBrands = await _repo.GetProductBrandsAsync();
+
+            return Ok(productsBrands);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            var productsTypes = await _repo.GetProductTypesAsync();
+
+            return Ok(productsTypes);
         }
 
         [HttpGet("{id}")]
