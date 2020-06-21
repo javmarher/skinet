0675a82436023d2e5d34ccce227e0935b516e8f1
jmh <javiermartinezherranz@gmail.com>
2020-06-21 13:51:54 +0200

    Exception handler middleware

--- API/Errors/ApiResponse.cs
+++ API/Errors/ApiResponse.cs
@@ -23,7 +23,7 @@ namespace API.Errors
             {
                 400 => "A bad request, you have made",
                 401 => "Autorized, you are not",
-                404 => "Resoruce found, it was not",
+                404 => "Resource found, it was not",
                 500 => "Errors are the path to the dark side. Errors lead to anger. Anger leads to career change",
                 _ => null
             };
