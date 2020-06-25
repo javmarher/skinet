c0b30c13f8baec6cfcea60440c4f0c48291d71e1
Javier Martinez <javier.martinez@iccat.int>
2020-06-25 10:06:44 +0200

    Finalizada API

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -22,14 +22,25 @@ namespace Core.Specifications
 
         public Expression<Func<T, object>> OrderBy { get; private set; }
 
-        public Expression<Func<T, object>> OrderByDescending { get; private set;  }
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
 
-        protected void AddInclude(Expression<Func<T, object>> includeExpression)
-        {
-            Includes.Add(includeExpression);
-        }
+        public int Take { get; private set; }
+
+        public int Skip { get; private set; }
+
+        public bool IsPagingEnabled { get; private set; }
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression) => Includes.Add(includeExpression);
 
         protected void AddOrderBy(Expression<Func<T, object>> orderByExpression) => OrderBy = orderByExpression;
-        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression) => OrderBy = orderByDescExpression;
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression) => OrderByDescending = orderByDescExpression;
+
+        protected void ApplyPaging(int skip, int take) 
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
+
+        }
     }
 }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,6 +1,7 @@
 ﻿
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,20 @@ namespace Infrastructure.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            if(Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach( var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach(var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name)
+                            .HasConversion<double>();
+                    }
+                }
+            }
         }
     }
 }
